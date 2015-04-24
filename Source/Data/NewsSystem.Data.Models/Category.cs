89e3217f545632d132cfb8c64efed0dd04a19df4
Dr4g0 <d_d0003@yahoo.com>
2015-04-24 09:36:50 +0300

    Added News model

---
+++ Source/Data/NewsSystem.Data.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsSystem.Data.Models
+{
+    public enum Category
+    {
+        World,
+        Bulgaria,
+        Business,
+        Tech,
+        Science,
+        Magazine,
+        Entertainment,
+        Health
+    }
+}
---
+++ Source/Data/NewsSystem.Data.Models/News.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace NewsSystem.Data.Models
+{
+    public class News
+    {
+        public News()
+        {
+            this.CreatedOn = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [Index(IsUnique=true)]
+        public string Link { get; set; }
+
+        public Category Category { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int CountVisits { get; set; }
+
+        public int CountPublications { get; set; }
+
+        public int CountUpdates { get; set; }
+    }
+}
