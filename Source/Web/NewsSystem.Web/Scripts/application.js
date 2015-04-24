ca5f2bd0be67e2f57228c8c0dc40ceb067ee556e
Dr4g0 <d_d0003@yahoo.com>
2015-04-24 16:19:55 +0300

    Added filter by date option

---
+++ Source/Web/NewsSystem.Web/Scripts/application.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('body').on('focus', ".datetimepicker", function () {
+        $(this).datetimepicker({
+            format: 'MM/DD/YYYY'
+        })
+    });
+})
