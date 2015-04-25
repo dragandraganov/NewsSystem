841b949024c98febd2beb6ca60f85c32f94df47f
Dr4g0 <d_d0003@yahoo.com>
2015-04-25 22:09:51 +0300

    Added graphs by criteria

---
+++ Source/Data/NewsSystem.Data.Models/GraphCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace NewsSystem.Data.Models
+{
+    public enum GraphCriteria
+    {
+        [Display(Name = "Most published news")]
+        MostPublished,
+
+        [Display(Name = "Most read news")]
+        MostRead,
+
+        [Display(Name = "News by Category")]
+        ByCategory,
+    }
+}
---
+++ Source/Web/NewsSystem.Web/Controllers/GraphsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using NewsSystem.Data;
+
+namespace NewsSystem.Web.Controllers
+{
+    public class GraphsController : BaseController
+    {
+        public GraphsController(NewsSystemData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index(DateTime? startDate, DateTime? endDate, string criteria)
+        {
+            var chart = new Chart(width: 1200, height: 400);
+
+            var allNews = this.Data.News.All();
+
+            if (startDate != null || endDate != null)
+            {
+                if (endDate == null)
+                {
+                    endDate = DateTime.Now;
+                }
+
+                allNews = allNews.Where(n => n.PubDate >= startDate && n.PubDate <= endDate);
+            }
+
+            switch (criteria)
+            {
+                case "0":
+                    var mostPublishedNews = allNews
+                        .OrderByDescending(n => n.CountPublications)
+                        .Take(5);
+
+                    chart.AddTitle("Top 5 published news");
+
+                    chart.AddSeries(
+                        chartType: "Column",
+                        xValue: mostPublishedNews.Select(n => n.Title).ToList(),
+                        yValues: mostPublishedNews.Select(n => n.CountPublications).ToList());
+                    break;
+
+                case "1":
+                    var mostReadNews = allNews
+                        .OrderByDescending(n => n.CountVisits)
+                        .Take(5);
+
+                    chart.AddTitle("Top 5 visited news");
+
+                    chart.AddSeries(
+                        chartType: "Column",
+                        xValue: mostReadNews.Select(n => n.Title).ToList(),
+                        yValues: mostReadNews.Select(n => n.CountVisits).ToList());
+                    break;
+
+                case "2":
+                    var newsGroupedByCategory = allNews
+                        .GroupBy(n => n.Category)
+                        .Select(gr => new 
+                        { 
+                            Category = gr.Key.ToString(), 
+                            Count = gr.Count() 
+                        })
+                        .OrderBy(gr=>gr.Count);
+
+                    chart.AddTitle("News by category");
+
+                    chart.AddSeries(
+                        chartType: "Pie",
+                        xValue: newsGroupedByCategory.Select(n => n.Category).ToList(),
+                        yValues: newsGroupedByCategory.Select(n => n.Count).ToList());
+                    break;
+                default:
+                    break;
+            }
+
+            var file = chart.GetBytes("png");
+            return File(file, "image/png");
+        }
+    }
+}
